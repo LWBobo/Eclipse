d1caaef8465f95727b63c7f33cf5809297ed86f4
qingyishi <1697668797@qq.com>
2019-11-01 11:54:10 +0800

    1

---
+++ Cookie/src/com/servlet/cookie/CookieServlet.java
@@ -0,0 +1,40 @@
+package com.servlet.cookie;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CookieServlet
+ */
+@WebServlet("/ck")
+public class CookieServlet extends HttpServlet {
+			@Override
+			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				req.setCharacterEncoding("utf-8");
+				resp.setContentType("text/html;charset=utf-8");
+				
+				Cookie c =  new Cookie("mouse", "logie");
+				Cookie c2 = new Cookie("key","cherry");
+				c2.setMaxAge(3600*24*3);
+				c2.setPath("/Cookie/gc");
+				
+				
+				
+				resp.addCookie(c);
+				resp.addCookie(c2);
+				resp.getWriter().write("Cookie学习!");
+				
+				
+				
+				
+				
+				
+				
+			}
+}
